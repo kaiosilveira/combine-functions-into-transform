45c1c8ecb442cc6467b61e9aec3c71799a184636
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-18 12:01:41 +0100

    enrich reading with baseCharge in the enrichReading function

--- src/reading/index.js
+++ src/reading/index.js
@@ -1,4 +1,5 @@
 const { cloneDeep } = require('lodash');
+const { baseRate } = require('../tax-utils');
 
 const reading = { customer: 'Ivan', quantity: 10, month: 5, year: 2017 };
 
@@ -8,7 +9,12 @@ function acquireReading() {
 
 function enrichReading(aReading) {
   const result = cloneDeep(aReading);
+  result.baseCharge = calculateBaseCharge(result);
   return result;
+}
+
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
 }
 
 module.exports = { acquireReading, enrichReading };
--- src/reading/index.test.js
+++ src/reading/index.test.js
@@ -1,14 +1,18 @@
 const { enrichReading } = require('.');
 
 describe('enrichReading', () => {
+  const inputReading = { customer: 'Ivan', quantity: 10, month: 5, year: 2017 };
+
   it('should return a copy of the original object', () => {
-    const inputReading = { customer: 'Ivan', quantity: 10, month: 5, year: 2017 };
-
     const resultingReading = enrichReading(inputReading);
-
     expect(resultingReading.customer).toEqual(inputReading.customer);
     expect(resultingReading.month).toEqual(inputReading.month);
     expect(resultingReading.year).toEqual(inputReading.year);
     expect(resultingReading.quantity).toEqual(inputReading.quantity);
+  });
+
+  it('should enrich the reading with the base charge', () => {
+    const resultingReading = enrichReading(inputReading);
+    expect(typeof resultingReading.baseCharge).toBe('number');
   });
 });
