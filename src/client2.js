5ee217be0758cd6ec9cd1f2f78b24c2935df96b2
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-18 12:10:14 +0100

    update client2 to use the taxableCharge field from the enriched reading obj

--- src/client2.js
+++ src/client2.js
@@ -1,10 +1,9 @@
 const { acquireReading, enrichReading } = require('./reading');
-const { taxThreshold } = require('./tax-utils');
 
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
 const base = aReading.baseCharge;
-const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const taxableCharge = aReading.taxableCharge;
 
 console.log(`base charge is ${base}`);
 console.log(`taxable charge is ${taxableCharge}`);
